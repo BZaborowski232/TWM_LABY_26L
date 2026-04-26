e3fb279ec9ae5773665414d8be620d5e39b178bf
Bartosz Zaborowski <zabobart@gmail.com>
2026-04-26 16:04:27 +0200

    LAB4 model update ~82% training accuracy 20 epok

---
+++ LAB4/model.py
@@ -0,0 +1,62 @@
+import tensorflow as tf 
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, Activation
+
+def generate_model():
+    model = Sequential()
+
+    # --- Pierwszy blok splotowy (32 filtry) ---
+    model.add(Conv2D(32, (3, 3), padding='same', input_shape=(32, 32, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    
+    model.add(Conv2D(32, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    # --- Drugi blok splotowy (64 filtry) ---
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    
+    model.add(Conv2D(64, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    # --- Trzeci blok splotowy (128 filtrów) - DODANY ---
+    # To jest klucz do osiągnięcia >80% dokładności
+    model.add(Conv2D(128, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    
+    model.add(Conv2D(128, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    # --- Klasyfikator na końcu sieci ---
+    model.add(Flatten())
+    
+    model.add(Dense(512))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    
+    # Warstwa wyjściowa - 10 klas (TYLKO RAZ!)
+    model.add(Dense(10))
+    model.add(Activation('softmax'))
+
+    model.summary()
+    
+    adam = tf.optimizers.Adam(learning_rate=0.001)
+    model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+
+    return model
